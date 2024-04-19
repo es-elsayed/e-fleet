2e88a29e676b06d51adc311c6a2151d89f29c4c6
islam-abdelkader <islamabdelkader22@gmail.com>
2024-04-19 05:27:48 +0200

    create form components

---
+++ resources/js/Components/Form/Label.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    value: {
+        type: String,
+    },
+    required: Boolean
+});
+</script>
+
+<template>
+    <label class="block text-sm font-medium text-gray-700">
+        <span v-if="value">{{ $t(value) }}</span>
+        <span v-if="required" class="text-red-600">&#160;*</span>
+        <span v-else><slot /></span>
+    </label>
+</template>
---
+++ resources/js/Components/Form/TextareaGroup.vue
@@ -0,0 +1,40 @@
+<script setup>
+import Input from "./Input.vue";
+import Label from "./Label.vue";
+import Error from "./Error.vue";
+
+defineProps({
+    modelValue: {},
+    type: {
+        type: String,
+        default: "text",
+    },
+    label: {
+        type: String,
+        default: "",
+    },
+    errorMessage: {
+        type: String,
+        default: "",
+    },
+    gClass: {
+        type: String,
+        default: "",
+    },
+    placeholder: {
+        type: String,
+        default: "",
+    }
+});
+</script>
+
+<template>
+    <div :class="gClass">
+        <Label v-if="label" :for="$attrs.id ?? $attrs.name" :value="label" />
+        <textarea
+            class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-pr-500 focus:ring-pr-500"
+            :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" ref="input" rows="10" :placeholder="$t(placeholder)"
+            v-bind="$attrs" />
+        <Error v-if="errorMessage" class="mt-1" :message="errorMessage" />
+    </div>
+</template>
