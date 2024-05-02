5868141e14430be62dd09d62da02dba6745dcd8e
es-elsayed <islamabdelkader22@gmail.com>
2024-05-02 11:48:24 +0300

    fix helper file laravel error

--- app/Helpers.php
+++ app/Helpers.php
@@ -2,23 +2,29 @@
 
 use App\Models\Place;
 
-function mapAllPlaces()
-{
-    $places = Place::pluck('name_en', 'id');
-    // Loop through each element of $arr1
-    foreach ($places as $from_id => $from) {
-        foreach ($places as $to_id => $to) {
-            // Skip if $from and $to are the same
-            if ($from_id === $to_id) {
-                continue;
+if (!function_exists('mapAllPlaces')) {
+
+    function mapAllPlaces()
+    {
+        $places = Place::pluck('name_en', 'id');
+        // Loop through each element of $arr1
+        $result = [];
+        if ($places && is_array($places)) {
+            foreach ($places as $from_id => $from) {
+                foreach ($places as $to_id => $to) {
+                    // Skip if $from and $to are the same
+                    if ($from_id === $to_id) {
+                        continue;
+                    }
+                    // Construct the new array element
+                    $result[] = [
+                        "from_place_id" => $from_id,
+                        "to_place_id" => $to_id,
+                        "is_active" => 1
+                    ];
+                }
             }
-            // Construct the new array element
-            $result[] = [
-                "from_place_id" => $from_id,
-                "to_place_id" => $to_id,
-                "is_active" => 1
-            ];
         }
+        return $result;
     }
-    return $result;
 }
